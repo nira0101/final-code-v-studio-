8e2b04c133f978a840c57cb859a2587e248b2789
nira0101 <nira0101@stud.kea.dk>
2020-12-21 13:01:20 +0100

    final touches

--- js/script.js
+++ js/script.js
@@ -1,5 +1,4 @@
-
-window.addEventListener("DOMContentLoaded",getData);
+window.addEventListener("DOMContentLoaded", getData);
 const link = "http://nirajan.dk/final-project/wp-json/wp/v2/v_studio?_embed";
 
 function getData() {
@@ -9,43 +8,43 @@ function getData() {
     const the_work_id = urlParams.get("v_studio_id");
     console.log(the_work_id);
 
-    if(the_work_id) {
+    if (the_work_id) {
         fetch("http://nirajan.dk/final-project/wp-json/wp/v2/v_studio/" + the_work_id + "?_embed")
-        .then(initial => initial.json())
-        .then(showWork)
-    }
-    else{
+            .then(initial => initial.json())
+            .then(showWork)
+    } else {
         fetch(link)
-.then(initial => initial.json())
-.then(callBack)
+            .then(initial => initial.json())
+            .then(callBack)
     }
 
 }
-function callBack(data){
+
+function callBack(data) {
     console.log(data);
     data.forEach(showWork);
 
 }
 
-function showWork(singleWork){
+function showWork(singleWork) {
     console.log(singleWork)
     const img_url = singleWork._embedded["wp:featuredmedia"][0].source_url;
-     const template = document.querySelector("template").content;
+    const template = document.querySelector("template").content;
     const clone = template.cloneNode(true);
     const elMain = document.querySelector(".work-template");
     const a = clone.querySelector(".works a");
     const divWorkDescription = clone.querySelector("#work-desc");
 
-   if(divWorkDescription){
-       divWorkDescription.innerHTML = singleWork.content.rendered;
-   }
-   clone.querySelector(".title").textContent = singleWork.title.rendered;
+    if (divWorkDescription) {
+        divWorkDescription.innerHTML = singleWork.content.rendered;
+    }
+    clone.querySelector(".title").textContent = singleWork.title.rendered;
     clone.querySelector(".works-image").src = img_url;
-    if(a){
-           a.href += singleWork.id;
+    if (a) {
+        a.href += singleWork.id;
     }
 
-     elMain.appendChild(clone);
+    elMain.appendChild(clone);
 }
 
 
@@ -55,14 +54,15 @@ const openMenu = document.querySelector(".openMenu");
 const closeMenu = document.querySelector(".closeMenu");
 
 
-openMenu.addEventListener("click",show);
-closeMenu.addEventListener("click",close);
+openMenu.addEventListener("click", show);
+closeMenu.addEventListener("click", close);
 
-function show(){
+function show() {
     mainMenu.style.display = "flex";
     mainMenu.style.top = "0";
 }
-function close(){
+
+function close() {
     mainMenu.style.top = "-100%";
 }
 
@@ -71,4 +71,3 @@ const imgBox = document.querySelectorAll(".imgBox");
 imgBox.forEach(popUp => popUp.addEventListener("click", () => {
     popUp.classList.toggle("active");
 }))
-
